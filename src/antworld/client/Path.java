5c02f3523ebd659cf49d779883ebac707f5ff4d4
hehaijin <hehaijin1981@gmail.com>
2016-12-05 20:57:28 -0700

    a public Path class using Coordinate class

---
+++ src/antworld/client/Path.java
@@ -0,0 +1,38 @@
+package antworld.client;
+
+import java.util.LinkedList;
+
+
+
+public class Path
+{
+
+  private LinkedList<Coordinate> path = new LinkedList<>();
+
+  Coordinate getNext()
+  {
+    return path.pop();
+  }
+
+  /**
+   * add to the path in reverse sequence.
+   * 
+   * @param node
+   */
+  void add(Coordinate node)
+  {
+    path.push(node);
+  }
+
+  int size()
+  {
+    return path.size();
+  }
+  
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+
+  }
+
+}
