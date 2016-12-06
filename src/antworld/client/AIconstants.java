c9e1d46e9a72392eb251d731118df0cb0ce957fd
hehaijin <hehaijin1981@gmail.com>
2016-12-05 22:57:09 -0700

    update

--- src/antworld/client/AIconstants.java
+++ src/antworld/client/AIconstants.java
@@ -1,12 +1,15 @@
 package antworld.client;
-
+/**
+ * 
+ * We can put some constants here for tweaking the performance of AI. 
+ *
+ */
 public class AIconstants
 {
+  public static final int antsPerFoodPile=10;
+  public static final boolean aggresive=false; // 0 means peaceful.
+  
+  
 
-  public static void main(String[] args)
-  {
-    // TODO Auto-generated method stub
-
-  }
 
 }
