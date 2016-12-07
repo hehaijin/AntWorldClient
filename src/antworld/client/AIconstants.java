a4905320bc42c4de306ce8cbb0674cbd49f81616
Hector A Carrillo Cabada <hcarrillo@unm.edu>
2016-12-06 19:12:14 -0700

    Added some constants to AIconstants

--- src/antworld/client/AIconstants.java
+++ src/antworld/client/AIconstants.java
@@ -8,8 +8,11 @@ public class AIconstants
 {
   public static final int antsPerFoodPile=10;
   public static final boolean aggresive=false; // 0 means peaceful.
-  
-  
+
+  // I may or may not end up using these
+  public static final int GENERAL_ANT_ATTACK_HEALTH_DIFF = 3;
+  public static final int ATTACK_ANT_ATTACK_HEALTH_DIFF = 3;
+  public static final int DEFENSE_ANT_ATTACK_HEALTH_DIFF = 3;
 
 
 }
